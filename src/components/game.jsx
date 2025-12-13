56200971066760c3335fc41859e75c2d3c266867
michaelprocha <michaelrocha9722@gmail.com>
2025-12-13 00:27:50 -0300

    feat: history of letters and focus on input

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Button from "./button";
 
 function Game({
-    setEndSection,
-    setOwnSection,
-    section,
+	setEndSection,
+	setOwnSection,
+	section,
 	resetWord,
 	score,
 	setScore,
@@ -15,8 +15,11 @@ function Game({
 	chances,
 	setChances,
 }) {
-    const game = section;
+
+	const game = section;
+	const inputRef = useRef(null)
 	const [guessLetter, setGuessLetter] = useState("");
+	const [letters, setLetters] = useState("")
 
 	const newWordView = [...viewWord];
 	const stringSecretWord = secretWord.join("");
@@ -28,9 +31,10 @@ function Game({
 	const guess = (e) => {
 		e.preventDefault();
 		let right = false;
+		const lowerCaseGuess = guessLetter.toLowerCase();
 		secretWord.forEach((letterAccent, i) => {
 			const letter = removerAcentos(letterAccent);
-			if (letter === guessLetter) {
+			if (letter === lowerCaseGuess) {
 				newWordView[i] = letterAccent;
 				right = true;
 			}
@@ -42,16 +46,33 @@ function Game({
 
 		const stringNewWord = newWordView.join("");
 
+		setGuessLetter("");
+
+		if (letters === "") {
+			setLetters(guessLetter);
+		}else{
+			setLetters(`${letters}, ${guessLetter}`);
+		}
+
+		inputRef.current.focus();
+
 		if (stringNewWord === stringSecretWord) {
+			setLetters("");
 			setScore(score + 100);
 			resetWord();
 		}
 
 		if (chances <= 1) {
+			setLetters("");
 			setOwnSection("hidden");
 			setEndSection("flex");
 		}
 	};
+
+	function formatInput(e){
+		const letter = e.target.value.toUpperCase();
+		setGuessLetter(letter);
+	}
 
 	return (
 		<section className={`my-auto ${game} flex-col gap-6 items-center`}>
@@ -66,21 +87,24 @@ function Game({
 				))}
 			</ul>
 			<p className="text-white font-bold">Tente adivinha uma letra da palavra:</p>
-			<form onSubmit={(e) => guess(e)}>
-				<label className="relative flex gap-4 items-center">
+			<form className="flex gap-4 items-center" onSubmit={(e) => guess(e)}>
+				<label className="relative">
 					<span className="absolute bottom-9999">Digite a letra</span>
 					<input
+						ref={inputRef}
+						autoComplete="off"
 						required
 						maxLength="1"
 						value={guessLetter}
-						onInput={(e) => setGuessLetter(e.target.value)}
+						onInput={(e) => formatInput(e)}
 						className="bg-white h-8 w-8 text-center text-2xl rounded-md"
 						type="text"
 						name="letter"
 					/>
-					<Button action={null} textButton={"Chutar"} />
 				</label>
+				<Button action={null} textButton={"Chutar"} />
 			</form>
+			<p className="text-white font-medium">Letras chutadas: {letters}</p>
 		</section>
 	);
 }
